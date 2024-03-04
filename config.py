f30b26634de381b531bf803808ca6885f2dabd2e
mini <39670899+minisbett@users.noreply.github.com>
2024-03-04 21:25:29 +0100

    refactor: improve config resiliency & handling (#13)

--- config.py
+++ config.py
@@ -1,80 +1,106 @@
-import os.path
 import json
 
-from typing import Any
+from typing import TypedDict, TypeVar
 from utils.logging import Color, log
 
-DEFAULT_HOTKEYS: list[str] = [
-    # file
-    "ctrl+s",
-    "ctrl+l",
-    "ctrl+shift+a",
-    # edit
-    "ctrl+z",
-    "ctrl+y",
-    "ctrl+x",
-    "ctrl+c",
-    "ctrl+v",
-    "del",
-    "ctrl+a",
-    "ctrl+d",
-    "ctrl+g",
-    "ctrl+h",
-    "ctrl+j",
-    "ctrl+>",
-    "ctrl+<",
-    "ctrl+shift+r",
-    "ctrl+shift+s",
-    # compose
-    "ctrl+shift+d",
-    "ctrl+shift+f",
-    # timing
-    "f6",
-    "ctrl+p",
-    "ctrl+shift+p",
-]
 
-DEFAULT_STYLE = {
-    # direction
-    "is_horizontal": True,
-    # colors
-    "chin-color": "#d9d9d9",
-    "shadow-color": "#adb5bd",
-    "text-color": "#343a40",
-    "subtext-color": "#777",
-    "border-color": "#e6e6e6",
-    "background-color": "#fff",
-    # animation settings
-    "animation-duration": "1s",
-    "animation-delay": "5s",
-    # font size
-    "font-size": "25px"
-}
-
-default_config: dict[str, Any] = {
-    "server_configuration": {
-        "port": 8000
-    },
-    "hotkey_style": DEFAULT_STYLE,
-    "hotkeys": DEFAULT_HOTKEYS
-}
-
-config: dict[str, Any] = {}
+class _HotkeyStyleConfig(TypedDict):
+    is_horizontal: bool
+    chin_color: str
+    shadow_color: str
+    text_color: str
+    subtext_color: str
+    border_color: str
+    background_color: str
+    animation_duration: str
+    animation_delay: str
+    font_size: str
 
 
-def load_config() -> None:
-    global config
-    if not os.path.isfile("config.json"):
-        with open("config.json", "w") as file:
-            json.dump(default_config, file, indent=2)
-            config = default_config
-            
-        log("No config.json found, created default config", Color.YELLOW)
-    else:
-        try:
-            with open("config.json", "r") as file:
-                config = json.load(file)
-        except json.decoder.JSONDecodeError as e:
-            log(f"Failed to parse the config file: {e}.", Color.RED)
-            log("Consider deleting it to create a default config.", Color.YELLOW)
-            raise SystemExit
+class Config(TypedDict):
+    port: int
+    hotkey_style: _HotkeyStyleConfig
+    hotkeys: list[str]
+
+
+_DEFAULT_CONFIG: Config = Config(
+    port=8000,
+    hotkey_style=_HotkeyStyleConfig(
+        is_horizontal=True,
+        chin_color="#d9d9d9",
+        shadow_color="#adb5bd",
+        text_color="#343a40",
+        subtext_color="#777",
+        border_color="#e6e6e6",
+        background_color="#fff",
+        animation_duration="1s",
+        animation_delay="5s",
+        font_size="25px",
+    ),
+    hotkeys=[
+        "ctrl+s",
+        "ctrl+l",
+        "ctrl+shift+a",
+        "ctrl+z",
+        "ctrl+y",
+        "ctrl+x",
+        "ctrl+c",
+        "ctrl+v",
+        "del",
+        "ctrl+a",
+        "ctrl+d",
+        "ctrl+g",
+        "ctrl+h",
+        "ctrl+j",
+        "ctrl+>",
+        "ctrl+<",
+        "ctrl+shift+r",
+        "ctrl+shift+s",
+        "ctrl+shift+d",
+        "ctrl+shift+f",
+        "f6",
+        "ctrl+p",
+        "ctrl+shift+p",
+    ],
+)
+
+
+def _load_config(filename: str) -> Config:
+    # load the config from the specified file
+    config = {}
+    try:
+        config = json.load(open(filename, "r"))
+    except json.decoder.JSONDecodeError as e:
+        log(f"Failed to parse the config file: {e}.", Color.RED)
+        log(
+            "Consider deleting the config.json file to create a new default config.",
+            Color.YELLOW,
+        )
+        raise SystemExit
+    except FileNotFoundError:
+        log("No config.json found, creating a default config.", Color.YELLOW)
+
+    # ensure default values for all unset properties recursively (sub-dicts)
+    _set_defaults_recursive(config, _DEFAULT_CONFIG)
+
+    # save the config in order to add properties missing in the file with their default values
+    json.dump(config, open(filename, "w"), indent=4)
+
+    return Config(Config(**config))
+
+
+T = TypeVar("T", bound=dict)
+
+
+def _set_defaults_recursive(dictionary: T, default: T) -> None:
+    # set the default value for all items of the specified defaults
+    for key, value in default.items():
+        dictionary.setdefault(key, value)
+
+        # if the value is a sub-dictionary, handle it recursively
+        if isinstance(value, dict):
+            dictionary.setdefault(key, {})
+            _set_defaults_recursive(dictionary[key], value)
+
+
+config: Config = _load_config("config.json")
